b0dbbdd096ce81ab5a27b566dafb032482e79a8f
Guilherme Quental <guilherme@quent.al>
2017-09-03 18:29:55 -0300

    Add logic to list pokemons

--- server/pokemon.go
+++ server/pokemon.go
@@ -5,6 +5,10 @@ import (
 
 	"github.com/gquental/pokedex/persistence"
 
+	"strconv"
+
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gquental/pokedex/data"
 	"gopkg.in/mgo.v2/bson"
@@ -45,6 +49,44 @@ func GetPokemonDetail(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"pokemon": pokemon, "damages": types})
+}
+
+func GetPokemonList(c *gin.Context) {
+	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+	if err != nil {
+		page = 1
+	}
+
+	session, collection, err := persistence.GetCollection("pokemons")
+	defer session.Close()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "It was not possible to connect to pokemon collection"})
+		return
+	}
+
+	pokemons := []data.Pokemon{}
+	err := collection.Find(bson.M{}).Limit(20).Skip(20 * page).All(&pokemons)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	count, err := collection.Count()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	pages := count / 20
+	nextPage := page + 1
+	next := fmt.Sprintf("%s%s%s%d", c.Request.URL.Scheme, c.Request.URL.Host, "pokemon?page=", nextPage)
+
+	if page == pages {
+		next = ""
+	}
+
+	c.JSON(http.StatusOK, gin.H{"count": count, "pokemons": pokemons, "next": next})
+
 }
 
 func getTypes(pokemon data.Pokemon) ([]data.DamageType, error) {
